99395d1750324140e552a64d01c5a320421865e3
Kenneth Troughton <kenneth.troughton@gmail.com>
2014-01-18 21:27:16 -0800

    commiting package.json and server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 5000;
+
+app.listen(port, function() {
+	console.log("Listening on " + port);
+});
+
+app.all("/hellowWorld", function(req, res)
+{
+	res.end("Hello, World!");
+});
